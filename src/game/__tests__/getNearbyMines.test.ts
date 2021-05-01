934d2dbede1857d0413b6810a2d5ee711923ba2d
Suhair Zain <suhair.zain@everreal.co>
2021-05-02 01:40:41 +0530

    Add getNearbyMines function

---
+++ src/game/__tests__/getNearbyMines.test.ts
@@ -0,0 +1,62 @@
+import { IBoard } from "../../interfaces/IBoardState";
+import { getNearbyMines } from "../getNearbyMines";
+import { formatBoard } from "./helpers";
+
+const inputAndExpected: {
+  input: { board: IBoard; position: number };
+  expectedMinesNearby: number;
+}[] = [
+  {
+    input: {
+      board: {
+        tiles: [
+          { isMine: false },
+          { isMine: false },
+          { isMine: false },
+          { isMine: false },
+        ],
+      },
+      position: 0,
+    },
+    expectedMinesNearby: 0,
+  },
+  {
+    input: {
+      board: {
+        tiles: [
+          { isMine: false },
+          { isMine: true },
+          { isMine: false },
+          { isMine: false },
+        ],
+      },
+      position: 0,
+    },
+    expectedMinesNearby: 1,
+  },
+  {
+    input: {
+      board: {
+        tiles: [
+          { isMine: true },
+          { isMine: true },
+          { isMine: true },
+          { isMine: true },
+        ],
+      },
+      position: 0,
+    },
+    expectedMinesNearby: 3,
+  },
+];
+
+describe("WHEN given getNearbyMines", () => {
+  for (const { expectedMinesNearby, input } of inputAndExpected) {
+    const { position, board } = input;
+    describe(`WHEN given ${formatBoard(board)}\nand ${position}`, () => {
+      it(`SHOULD be ${expectedMinesNearby}`, () => {
+        expect(getNearbyMines(board, position)).toBe(expectedMinesNearby);
+      });
+    });
+  }
+});
---
+++ src/game/getNearbyMines.ts
@@ -0,0 +1,9 @@
+import { IBoard, ITile } from "../interfaces/IBoardState";
+import { getNearbyTiles } from "./getNearbyTiles";
+
+export const getNearbyMines = (board: IBoard, position: number) => {
+  return getNearbyTiles(board, position).reduce(
+    (acc, p) => acc + (board.tiles[p].isMine ? 1 : 0),
+    0
+  );
+};
